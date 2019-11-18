3e45d8288adef25ee82358a2786eaebd21c44fe8
Gideon Tong <gideon@gideontong.com>
2019-11-17 18:25:34 -0800

    A little more details

--- analyze-details.py
+++ analyze-details.py
@@ -2,4 +2,18 @@ import json
 
 with open('details-0.json', 'r+') as file:
     data = json.load(file)
-    print(data['products'][0]['brandName'])
+    """
+    print(len(data['products'][0]))
+    for key in data['products'][0]:
+        print(key)
+    """
+
+    #print(len(data['priceHasError']))
+    print(data['totalCount'])
+
+    """
+    for key in data['coupons']:
+        print(key)
+    """
+
+    #print(data['products'][0]['brandName'])
